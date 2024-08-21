ca04d62d564627bb67ce82fb2b4928fd5bbc150d
sejalbhokare <sejalbhokare02@gmail.com>
2024-08-21 15:58:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import LoginForm from './LoginForm'
+
+function App() {
+
+  return (
+    <>
+    
+
+    <div className='text-white h-[100vh] flex justify-center items-center bg-cover'>
+      <LoginForm/>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
